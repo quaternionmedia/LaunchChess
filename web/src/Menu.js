64cc2c360750c82751accc783d1fef27c48211ac
Harpo <harpo@harpo.me>
2021-01-26 11:59:42 -0800

    base web template

---
+++ web/src/Menu.js
@@ -0,0 +1,42 @@
+import m from 'mithril'
+
+
+export function Link() {
+  return {
+    view: (vnode) => {
+      return m('.menu-item', [
+        m(m.route.Link, vnode.attrs, vnode.children)
+      ])
+    }
+  }
+}
+
+export function Links() {
+  return {
+    view: vnode => {
+      return [
+        m(Link, {href:'/connect', id: 'connect'}, 'connect'),
+      ]
+    }
+  }
+}
+
+export function Menu() {
+  return {
+    view: vnode => {
+      return [m(Link, {href: '/'}, 'magnus'),
+      m(Links),]
+    }
+  }
+}
+
+export function Layout() {
+  return {
+    view: vnode => {
+      return m('main.layout', {}, [
+        m('nav.menu', {}, m(Menu)),
+        m('section', vnode.children)
+      ])
+    }
+  }
+}
---
+++ web/src/index.js
@@ -0,0 +1,44 @@
+import m from 'mithril'
+import { Layout } from './Menu'
+import WebMidi from 'webmidi'
+
+import './style.css'
+
+export function Home() {
+  return {
+    view: vnode => {
+      return m('#home', {}, 'magnus')
+    }
+  }
+}
+export function Connect() {
+  function 
+  return {
+    oninit: vnode => {
+      WebMidi.enable(function (err) {
+        console.log(WebMidi.inputs);
+        console.log(WebMidi.outputs);
+        var input = WebMidi.getInputByName('Launchpad X MIDI 2')
+        var output = WebMidi.getOutputByName('Launchpad X MIDI 2')
+        console.log(input, output)
+        output.playNote('F4')
+        input.addListener('noteon', "all",
+        function (e) {
+          console.log("Received 'noteon' message (" + e.note.name + e.note.octave + ").");
+        }
+      );
+      });
+    },
+    
+    view: vnode => {
+      return m('#connect', {}, 'connect')
+    }
+  }
+}
+console.log('magnus started!')
+
+m.route(document.body, '/', {
+  '/': { render: () => m(Layout, m(Home))},
+  '/connect': { render: () => m(Layout, m(Connect))},
+
+})
