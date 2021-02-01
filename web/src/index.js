3139968e4bdafd824bc4885c2da980ebd594733b
Harpo <harpo@harpo.me>
2021-02-01 12:53:23 -0800

    basic connected / disconnected component!

--- web/src/index.js
+++ web/src/index.js
@@ -12,25 +12,42 @@ export function Home() {
   }
 }
 export function Connect() {
+  var input, output
+  var connected = false
   return {
     oninit: vnode => {
       WebMidi.enable(function (err) {
-        console.log(WebMidi.inputs);
-        console.log(WebMidi.outputs);
-        var input = WebMidi.getInputByName('Launchpad X MIDI 2')
-        var output = WebMidi.getOutputByName('Launchpad X MIDI 2')
-        console.log(input, output)
-        output.playNote('F4')
+        console.log(WebMidi.inputs)
+        console.log(WebMidi.outputs)
+        WebMidi.addListener("connected", function(e) {
+          console.log('midi connected', e, e.port.name)
+          if (e.port.name == 'Launchpad X MIDI 2') {
+            if (e.port.type == 'output') {
+              output = e.port
+              console.log('output', output)
+              output.playNote('F4')
+            } else {
+              input = e.port
+            }
+          }
+        })
+        WebMidi.addListener("disconnected", function(e) {
+        console.log('midi disconnected', e)
+        })
+        // var input = WebMidi.getInputByName('Launchpad X MIDI 2')
+        // var output = WebMidi.getOutputByName('Launchpad X MIDI 2')
+        
         input.addListener('noteon', "all",
         function (e) {
-          console.log("Received 'noteon' message (" + e.note.name + e.note.octave + ").");
+          console.log("Received 'noteon' message (" + e.note.name + e.note.octave + ").")
         }
-      );
-      });
+      )
+      console.log(input, output)
+      })
     },
     
     view: vnode => {
-      return m('#connect', {}, 'connect')
+      return m('#connect', {}, connected ? 'connected!' : 'disconnected')
     }
   }
 }
