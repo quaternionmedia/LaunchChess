9a4fc1b732bd800b0d19804072ac1b37a59e0615
Harpo <harpo@harpo.me>
2021-02-01 12:57:32 -0800

    moved midiConnect to own function

--- web/src/index.js
+++ web/src/index.js
@@ -4,6 +4,8 @@ import WebMidi from 'webmidi'
 import { Board } from './Board'
 import './style.css'
 
+const deviceName = 'Launchpad X MIDI 2'
+
 export function Home() {
   return {
     view: vnode => {
@@ -14,40 +16,49 @@ export function Home() {
 export function Connect() {
   var input, output
   var connected = false
+  
+  function midiConnect() {
+    WebMidi.enable(function (err) {
+      console.log(WebMidi.inputs)
+      console.log(WebMidi.outputs)
+      WebMidi.addListener("connected", function(e) {
+        // console.log('midi connected', e, e.port.name)
+        if (e.port.name == deviceName) {
+          if (e.port.type == 'output') {
+            output = e.port
+            console.log('output', output)
+            output.playNote('F4')
+          } else {
+            input = e.port
+            console.log('input', input)
+            input.addListener('noteon', "all", e => {
+              console.log("Received 'noteon' message", e.note.name + e.note.octave, e)
+            })
+          }
+          m.redraw()
+        }
+      })
+      WebMidi.addListener("disconnected", function(e) {
+        console.log('midi disconnected', e)
+        if (e.port.name == deviceName) {
+          if (e.port.type == 'output') {
+            output = null
+          } else {
+            input = null
+          }
+          m.redraw()
+        }
+      })
+    })
+  }
+
   return {
     oninit: vnode => {
-      WebMidi.enable(function (err) {
-        console.log(WebMidi.inputs)
-        console.log(WebMidi.outputs)
-        WebMidi.addListener("connected", function(e) {
-          console.log('midi connected', e, e.port.name)
-          if (e.port.name == 'Launchpad X MIDI 2') {
-            if (e.port.type == 'output') {
-              output = e.port
-              console.log('output', output)
-              output.playNote('F4')
-            } else {
-              input = e.port
-            }
-          }
-        })
-        WebMidi.addListener("disconnected", function(e) {
-        console.log('midi disconnected', e)
-        })
-        // var input = WebMidi.getInputByName('Launchpad X MIDI 2')
-        // var output = WebMidi.getOutputByName('Launchpad X MIDI 2')
-        
-        input.addListener('noteon', "all",
-        function (e) {
-          console.log("Received 'noteon' message (" + e.note.name + e.note.octave + ").")
-        }
-      )
-      console.log(input, output)
-      })
+      midiConnect()
     },
     
     view: vnode => {
-      return m('#connect', {}, connected ? 'connected!' : 'disconnected')
+      return m('#connect', {}, input && output ? 'connected!' : 'disconnected')
     }
   }
 }
