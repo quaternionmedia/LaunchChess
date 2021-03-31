4fc579754620796bd13ba387d32f8f94d95026e5
Harpo <harpo@harpo.me>
2021-03-31 15:36:41 -0700

    changed logout to deauthorize, with confirm

--- web/src/Profile.js
+++ web/src/Profile.js
@@ -1,14 +1,22 @@
 import m from 'mithril'
 import { User } from './User'
-export function Profile() {
+import { confirm, message } from 'alertifyjs'
+
+export function ProfilePage() {
   return {
     view: vnode => {
       return [
         m('.profile', vnode.attrs, JSON.stringify(User.profile)),
-        m('button.button', {onclick: e => {
-          User.logout()
-          m.route.set('/')
-        }}, 'logout'),
+        m('i', {onclick: e => {
+          confirm('Are you sure you want to deauthorize this device?', affirm => {
+            User.logout()
+            m.route.set('/')
+            message('logged out', 3)
+          }, deny => {
+            console.log('That was a close one!')
+          })
+          
+        }}, 'deauthorize this device'),
       ]
     }
   }
