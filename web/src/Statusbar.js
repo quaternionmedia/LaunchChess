8ba5d0e271a423f4f2056433aa4e74cbfab36f08
Harpo <harpo@harpo.me>
2021-04-17 11:26:25 -0700

    simplified statusbar to be stateless!

--- web/src/Statusbar.js
+++ web/src/Statusbar.js
@@ -1,11 +1,7 @@
 import m from 'mithril'
 
 
-export const Statusbar = (state) => ({
-  view: vnode => {
-    let status = state.connected ? 'connected' : 'disconnected'
-    return m('.statusbar', {}, [
-    m('.status', {class: status, title: status}, ''),
-    m('', {}, status),
-  ])}
-})
+export const Statusbar = (state) => m('.statusbar', {}, [
+    m('.status', {class: state.connected() ? 'connected' : 'disconnected', title: state.connected() ? 'connected' : 'disconnected'}, ''),
+    m('', {}, state.connected() ? 'connected' : 'disconnected'),
+  ])
