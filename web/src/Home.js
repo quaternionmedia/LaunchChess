97de6e1ac810b937e326cbd3cbb047abe817591d
Harpo <harpo@harpo.me>
2021-05-18 17:48:15 -0700

    added web details to homepage

--- web/src/Home.js
+++ web/src/Home.js
@@ -1,15 +1,13 @@
 import m from 'mithril'
 import { Connector, ConnectionPage } from './Connector'
 
-export const website = 'https://quaternion.media/launchchess'
-
 
 export const Home = (state, actions) => ({
   view: () => m('#home', {}, [
     m('h1#home', {}, 'LaunchChess.com'),
     m('h4#home', {}, ['Play chess on your ', m('a', {href: 'https://novationmusic.com/en/launch/'}, 'Novation Launchpad')]),
     m('br'),
-    m('', {}, ['More details at the project homepage: ', m('a', {href: website}, website)]),
+    m('', {}, ['More details at the project homepage: ', m('a', {href: 'https://quaternion.media/launchchess'}, 'quaternion.media/launchchess')]),
     m('br'),
     m(ConnectionPage(state, actions)),
     m('br'),
