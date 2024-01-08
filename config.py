2207f910ba6fe57e651a4f136bf4815a5ac153bb
Harpo <harpo@harpo.me>
2024-01-07 17:23:34 -0800

    Moves web package to top level directory

--- config.py
+++
@@ -1,8 +0,0 @@
-SECRET_KEY=''
-PRODUCTION=False
-SESSION_SECRET=''
-AUTH_SECRET_KEY=''
-STOCKFISH='./stockfish_20090216_x64_avx2'
-LICHESS_AUTHORIZE_URL='https://lichess.org/oauth/'
-LICHESS_ACCESS_TOKEN_URL='https://lichess.org/api/token'
-LICHESS_API_URL='https://lichess.org/api'
--- db.py
+++
@@ -1,4 +0,0 @@
-from tinydb import TinyDB
-
-db = TinyDB('db.json')
-users = db.table('users')
